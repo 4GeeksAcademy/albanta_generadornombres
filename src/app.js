6b059689cacdb3b5b026ffc7225472b1ce2255c0
ALBANTA LD <albanta.leondelgado@gmail.com>
2024-11-14 10:27:40 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronombre = ["el", "nuestro", "vuestro"];
+  const adjetivo = ["gran", "fantástico", "increíble"];
+  const nombre = ["jugador", "profesor", "programador"];
+
+  const combinaciones = [];
+
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < adjetivo.length; j++) {
+      for (let k = 0; k < nombre.length; k++) {
+        combinaciones.push(`${pronombre[i]} ${adjetivo[j]} ${nombre[k]}`);
+      }
+    }
+  }
+
+  console.log(combinaciones);
 };
